4a3477cb306759a1d69e00e6256393340a61ef77
Harry Wang <hardhardcoding@gmail.com>
2019-06-14 13:31:00 +0800

    增加对mongodb的操作

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
-/*
- * app.js - Express server with routes module
-*/
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-/*jslint         node    : true, continue : true,
-  devel  : true, indent  : 2,    maxerr   : 50,
-  newcap : true, nomen   : true, plusplus : true,
-  regexp : true, sloppy  : true, vars     : false,
-  white  : true
-*/
-/*global */
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-// ------------ BEGIN MODULE SCOPE VARIABLES --------------
-'use strict';
-var
-  http    = require( 'http'     ),
-  express = require( 'express'  ),
-  routes  = require( './routes' ),
+// Database Name
+const dbName = 'myproject';
 
-  app     = express(),
-  server  = http.createServer( app );
-// ------------- END MODULE SCOPE VARIABLES ---------------
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
 
-// ------------- BEGIN SERVER CONFIGURATION ---------------
-app.configure( function () {
-  app.use( express.bodyParser() );
-  app.use( express.methodOverride() );
-  app.use( express.static( __dirname + '/public' ) );
-  app.use( app.router );
+  const db = client.db(dbName);
+
+  client.close();
 });
-
-app.configure( 'development', function () {
-  app.use( express.logger() );
-  app.use( express.errorHandler({
-    dumpExceptions : true,
-    showStack      : true
-  }) );
-});
-
-app.configure( 'production', function () {
-  app.use( express.errorHandler() );
-});
-
-routes.configRoutes( app, server );
-// -------------- END SERVER CONFIGURATION ----------------
-
-// ----------------- BEGIN START SERVER -------------------
-server.listen( 3000 );
-console.log(
-  'Express server listening on port %d in %s mode',
-   server.address().port, app.settings.env
-);
-// ------------------ END START SERVER --------------------
